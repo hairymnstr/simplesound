9a493c0a3245d352149648caa827805ffe53de6a
Nathan Dumont <nathan@nathandumont.com>
2016-11-21 22:35:42 +0000

    Cleaned up and annotated the source code to make it more useful as an example.

--- simplesound.py
+++ simplesound.py
@@ -3,34 +3,73 @@ import pygame, numpy
 class Tone:
     def __init__(self):
         """ Create a new tone generator """
+        # only initialise pygame if it hasn't been already.  Should make this
+        # play well with other pygame stuff
         if pygame.mixer.get_init() == None:
             pygame.init()
+        # load the parameters of the sound card
         self.sample_rate, self.format, self.channels = pygame.mixer.get_init()
-    
-    def _make_sample(self, freq):
-        if self.format < 0:
-            peak = 2 ** (abs(self.format)-1)
-        else:
-            peak = 2 ** self.format
-        omega = numpy.pi * 2 * freq / self.sample_rate
-        xvalues = numpy.arange(int(self.sample_rate)) * omega
-        return (peak * numpy.sin(xvalues))
     
     def play(self, freq, period_ms):
         """ Play a tone of specified frequency for a period of time.
             freq = frequency in Hz
             period_ms = time to play for in millisecods """
-        smp = self._make_sample(freq)
         
+        # 2 * pi * freq / sample_rate is the step between samples in radians
+        omega = numpy.pi * 2 * freq / self.sample_rate
+
+        # arange returns a vector of values we can multiply by our step value
+        # in one step using some of numpys mathematical optimisations
+        xvalues = numpy.arange(int(self.sample_rate)) * omega
+
+        # the values are scaled -1 to 1 as floating point, but we need to 
+        # scale them based on the sample format of the sound card
+        if self.format < 0:
+            smp = (2 ** (abs(self.format)-1)) * numpy.sin(xvalues)
+        else:
+            smp = (2 ** (abs(self.format)-1)) * (numpy.sin(xvalues) + 1)
+
+        # if the mixer is configured in stereo we need to copy the sine wave to
+        # both left and right channels
         if self.channels == 2:
-            smp = numpy.array(zip(smp, smp)).astype(numpy.int16)
+            smp = numpy.array(zip(smp, smp))
+
+        # the sound driver requires the numpy array to be presented in the same
+        # format.  This is because underneath the python there's a bunch of 
+        # C optimisations for speed.  Currently only support 8 or 16 bit samples
+        # in signed or unsigned format.  Could do more but I don't know what
+        # pygame supports.
+        if self.format == -16:
+            smp = smp.astype(numpy.int16)
+        elif self.format == 16:
+            smp = smp.astype(numpy.uint16)
+        elif self.format == -8:
+            smp = smp.astype(numpy.int8)
+        elif self.format == 8:
+            smp = smp.astype(numpy.uint8)
+        else:
+            raise Exception("Unhandled sample format")
+
+        # call this pygame function to convert the numpy array to a sound object
         snd = pygame.sndarray.make_sound(smp)
+
+        # the sinewave we created is 1 second long, so if the period requested
+        # is longer than that we need to loop.
         loops = period_ms / 1000
         if period_ms % 1000:
             loops += 1
+
+        # the loops parameter is odd in that it always plays once then does loops
+        # repeats, so 0 means play once, 1 means play twice etc.
         loops -= 1
         
+        # play the sine wave looping until the requested period is reached
         snd.play(loops, period_ms)
+
+        # play is an asynchronous call, that means it queues the sound up to be
+        # played and then returns immediately.  If we don't wait for it to play
+        # here you could end up playing all your notes at once and then quitting
+        # before any have actually played!
         pygame.time.delay(period_ms)
 
 if __name__ == "__main__":
